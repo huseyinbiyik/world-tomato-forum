ce7426b3c18b394a372605e247e7cb0d98f33341
Huseyinbiyik <tahahuseyinb@gmail.com>
2022-04-13 11:54:07 +0300

    Fix eslint issues

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//mobile navigation
-let navigation = document.querySelector('.mobile-nav');
-let menuButton = document.querySelector('.hamburger-menu');
-let closeButton = document.querySelector('.close-button');
+// mobile navigation
+const navigation = document.querySelector('.mobile-nav');
+const menuButton = document.querySelector('.hamburger-menu');
+const closeButton = document.querySelector('.close-button');
 
 menuButton.addEventListener('click', () => {
   navigation.style.display = 'flex';
@@ -11,59 +11,58 @@ closeButton.addEventListener('click', () => {
   navigation.style.display = 'none';
 });
 
-let links = document.querySelectorAll('.link');
+const links = document.querySelectorAll('.link');
 
-Array.from(links).forEach(link => {
+Array.from(links).forEach((link) => {
   link.addEventListener('click', () => {
     navigation.style.display = 'none';
-  })
+  });
 });
 
-//mobile navigation
+// mobile navigation
 
-//speakers section
-let speakersArray = [{
-    speakerImg: 'assets/speaker1.jpeg',
-    speakerName: 'Sarlos Cainz',
-    speakerTitle: 'Expert Tomato Farmer',
-    speakerAbout: 'Started to learn about tomato farming in the early 2000s. He has 5 books at home.',
-  },
-  {
-    speakerImg: 'assets/speaker2.jpeg',
-    speakerName: 'Dalvador Sali',
-    speakerTitle: 'Professor of Farming Technologies',
-    speakerAbout: 'He is a professor of farming technologies at the University of California, San Diego. He is the author of the book "The Science of Farming"',
-  },
-  {
-    speakerImg: 'assets/speaker3.jpeg',
-    speakerName: 'Ciguel de Mervantes',
-    speakerTitle: 'Blogger and Farmer for more than 60 years',
-    speakerAbout: 'With more than 60 years of experience in the field of farming, he is a blogger and farmer."',
-  },
-  {
-    speakerImg: 'assets/speakerwoman1.jpeg',
-    speakerName: 'Cenelope Pruz',
-    speakerTitle: 'Farming Technology Expert',
-    speakerAbout: 'Technology and farming is my passion. I have been a farmer since I was a child. I have been a farmer since I was a child.',
-  },
-  {
-    speakerImg: 'assets/speakerwoman2.jpeg',
-    speakerName: 'Dana de Armas',
-    speakerTitle: 'Thinker and Economist',
-    speakerAbout: 'I think about the future of the world and the future of the world. I am an economist.',
-  },
-  {
-    speakerImg: 'assets/speakerwoman3.jpeg',
-    speakerName: 'Sea Leydoux',
-    speakerTitle: 'Biological Engineer at the University of California',
-    speakerAbout: 'More than 400 conferences and workshops, one of the most influential people in the world of bioengineering. Nature 2021',
-  },
-]
+// speakers section
+const speakersArray = [{
+  speakerImg: 'assets/speaker1.jpeg',
+  speakerName: 'Sarlos Cainz',
+  speakerTitle: 'Expert Tomato Farmer',
+  speakerAbout: 'Started to learn about tomato farming in the early 2000s. He has 5 books at home.',
+},
+{
+  speakerImg: 'assets/speaker2.jpeg',
+  speakerName: 'Dalvador Sali',
+  speakerTitle: 'Professor of Farming Technologies',
+  speakerAbout: 'He is a professor of farming technologies at the University of California, San Diego. He is the author of the book "The Science of Farming"',
+},
+{
+  speakerImg: 'assets/speaker3.jpeg',
+  speakerName: 'Ciguel de Mervantes',
+  speakerTitle: 'Blogger and Farmer for more than 60 years',
+  speakerAbout: 'With more than 60 years of experience in the field of farming, he is a blogger and farmer."',
+},
+{
+  speakerImg: 'assets/speakerwoman1.jpeg',
+  speakerName: 'Cenelope Pruz',
+  speakerTitle: 'Farming Technology Expert',
+  speakerAbout: 'Technology and farming is my passion. I have been a farmer since I was a child. I have been a farmer since I was a child.',
+},
+{
+  speakerImg: 'assets/speakerwoman2.jpeg',
+  speakerName: 'Dana de Armas',
+  speakerTitle: 'Thinker and Economist',
+  speakerAbout: 'I think about the future of the world and the future of the world. I am an economist.',
+},
+{
+  speakerImg: 'assets/speakerwoman3.jpeg',
+  speakerName: 'Sea Leydoux',
+  speakerTitle: 'Biological Engineer at the University of California',
+  speakerAbout: 'More than 400 conferences and workshops, one of the most influential people in the world of bioengineering. Nature 2021',
+},
+];
 
 const speakers = document.querySelector('.speaker-container');
 
-
-for (let i = 0; i < speakersArray.length; i++) {
+for (let i = 0; i < speakersArray.length; i + 1) {
   speakers.innerHTML += `
     <div class="speakers-card">
     <div class="speakers-image">
@@ -77,5 +76,5 @@ for (let i = 0; i < speakersArray.length; i++) {
         ${speakersArray[i].speakerAbout}
       </p>
     </div>
-    </div>`
+    </div>`;
 }
